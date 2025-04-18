5f57a2e6d5d71bfecc67e0996046421f6c5507da
Caraxi <git@caraxian.com>
2025-04-18 14:33:34 +0930

    Update DebugWindow.cs

--- SimpleGlamourSwitcher/UserInterface/Windows/DebugWindow.cs
+++ SimpleGlamourSwitcher/UserInterface/Windows/DebugWindow.cs
@@ -1,5 +1,8 @@
-﻿using Dalamud.Interface.Utility.Raii;
+﻿using System.Numerics;
+using Dalamud.Interface.Colors;
+using Dalamud.Interface.Utility.Raii;
 using Dalamud.Interface.Windowing;
+using FFXIVClientStructs.FFXIV.Client.Game.UI;
 using Glamourer.Api.Enums;
 using ImGuiNET;
 using SimpleGlamourSwitcher.Configuration.Files;
@@ -8,10 +11,12 @@ using SimpleGlamourSwitcher.IPC;
 
 namespace SimpleGlamourSwitcher.UserInterface.Windows;
 
-public class DebugWindow() : Window("Simple Glamour Switcher Debug") {
+public unsafe class DebugWindow() : Window("Simple Glamour Switcher Debug") {
 
 
     private OrderedDictionary<Guid, OutfitConfigFile> outfits = new();
+
+    private CustomizePlus.ErrorCode LastCustomizePlusError = CustomizePlus.ErrorCode.Success;
     
     public override void Draw() {
 
@@ -58,19 +63,57 @@ public class DebugWindow() : Window("Simple Glamour Switcher Debug") {
                 
                 ImGui.EndTable();
             }
-            
-           
         }
-        
-        
-        
-        
-
-        
-        
-        
 
 
+        if (ImGui.CollapsingHeader("Customize+")) {
 
+            if (CustomizePlus.IsReady()) {
+                
+                ImGui.Text("Ready");
+
+                var chr = ClientState.LocalPlayer;
+                if (chr == null) {
+                    ImGui.TextUnformatted("No Character");
+                } else {
+
+                    if (CustomizePlus.TryGetActiveProfileOnCharacter(0, out var data)) {
+                        ImGui.TextUnformatted($"Active Profile: {data.Name}");
+                        ImGui.SameLine();
+                        ImGui.TextDisabled($"{data.UniqueId}");
+                    }
+                    
+                    var profiles = CustomizePlus.GetProfileList();
+
+                    foreach (var p in profiles) {
+                        using (ImRaii.PushId(p.UniqueId.ToString())) {
+                            var hasActiveCharacter = p.Characters.Any(c => c is { CharacterType: 1, CharacterSubType: 0 } && c.Name == chr.Name.TextValue && (c.WorldId == chr.HomeWorld.RowId || c.WorldId == ushort.MaxValue));
+                            
+                            if (hasActiveCharacter) {
+                                using (ImRaii.PushColor(ImGuiCol.Text, ImGuiColors.DalamudRed)) {
+                                    if (ImGui.Button("Remove Self", new Vector2(100, ImGui.GetTextLineHeightWithSpacing()))) {
+                                        CustomizePlus.TryRemovePlayerCharacterFromProfile(p.UniqueId, chr.Name.TextValue, chr.HomeWorld.RowId);
+                                        CustomizePlus.TryRemovePlayerCharacterFromProfile(p.UniqueId, chr.Name.TextValue, ushort.MaxValue);
+                                    }
+                                }
+                                
+                            } else {
+                                using (ImRaii.PushColor(ImGuiCol.Text, ImGuiColors.ParsedGreen)) {
+                                    if (ImGui.Button("Add Self", new Vector2(100, ImGui.GetTextLineHeightWithSpacing()))) {
+                                        CustomizePlus.TryAddPlayerCharacterToProfile(p.UniqueId, chr.Name.TextValue, chr.HomeWorld.RowId);
+                                    }
+                                }
+                                
+                            }
+                            ImGui.SameLine();
+                            ImGui.TextColored(ImGui.ColorConvertU32ToFloat4(p.IsEnabled ? ImGui.GetColorU32(ImGuiCol.Text) :  ImGui.GetColorU32(ImGuiCol.TextDisabled)), p.Name);
+
+                        }
+                    }
+                }
+            } else {
+                ImGui.Text("Compatible C+ version not detected.");
+            }
+        }
     }
 }

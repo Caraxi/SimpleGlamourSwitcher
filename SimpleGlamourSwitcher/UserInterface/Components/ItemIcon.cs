c12c2fe1d81ea3a6b535524ba3b119dd84b2ff71
Caraxi <git@caraxian.com>
2025-04-20 20:03:50 +0930

    Fix Item Icon Crash

--- SimpleGlamourSwitcher/UserInterface/Components/ItemIcon.cs
+++ SimpleGlamourSwitcher/UserInterface/Components/ItemIcon.cs
@@ -7,37 +7,20 @@ using Penumbra.GameData.Structs;
 namespace SimpleGlamourSwitcher.UserInterface.Components;
 
 public static class ItemIcon {
-    private static readonly Dictionary<HumanSlot, Action<HumanSlot, EquipItem, Vector2>> EmptyIcons = new();
-
-    static ItemIcon() {
-        EmptyIcons[HumanSlot.Head] = GetDrawBlankIcon(19);
-        EmptyIcons[HumanSlot.Body] = GetDrawBlankIcon(20);
-        EmptyIcons[HumanSlot.Hands] = GetDrawBlankIcon(21);
-        EmptyIcons[HumanSlot.Legs] = GetDrawBlankIcon(23);
-        EmptyIcons[HumanSlot.Feet] = GetDrawBlankIcon(24);
-        EmptyIcons[HumanSlot.Ears] = GetDrawBlankIcon(25);
-        EmptyIcons[HumanSlot.Neck] = GetDrawBlankIcon(26);
-        EmptyIcons[HumanSlot.Wrists] = GetDrawBlankIcon(27);
-        EmptyIcons[HumanSlot.RFinger] = GetDrawBlankIcon(28);
-        EmptyIcons[HumanSlot.LFinger] = GetDrawBlankIcon(28);
-
-        EmptyIcons[HumanSlot.Face] = GetDrawBlankIcon(55);
-    }
-
-    private static Action<HumanSlot, EquipItem, Vector2> GetDrawBlankIcon(int part) {
-        return (_, _, size) => {
-            using var characterUld = UiBuilder.LoadUld("ui/uld/Character.uld");
-            var tex = characterUld.LoadTexturePart("ui/uld/Character_hr1.tex", part);
-            ImGui.Dummy(size);
-            var dl = ImGui.GetWindowDrawList();
-            dl.AddRectFilled(ImGui.GetItemRectMin(), ImGui.GetItemRectMax(), ImGui.GetColorU32(ImGuiCol.FrameBg), size.X * 0.15f, ImDrawFlags.RoundCornersAll);
-            if (tex != null)
-                dl.AddImage(tex.ImGuiHandle, ImGui.GetItemRectMin(), ImGui.GetItemRectMax());
-            else
-                dl.AddText(ImGui.GetItemRectMin(), 0xFF0000FF, "x");
-        };
-    }
-
+    private static readonly Dictionary<HumanSlot, (string TexturePath, Vector2 uvMin, Vector2 uvMax)> EmptyIcons = new() {
+        { HumanSlot.Head, ("ui/uld/Character_hr1.tex", new Vector2(0.2500f, 0.3273f), new Vector2(0.3750f, 0.4727f)) },
+        { HumanSlot.Body, ("ui/uld/Character_hr1.tex", new Vector2(0.3750f, 0.3273f), new Vector2(0.5000f, 0.4727f)) },
+        { HumanSlot.Hands, ("ui/uld/Character_hr1.tex", new Vector2(0.5000f, 0.3273f), new Vector2(0.6250f, 0.4727f)) },
+        { HumanSlot.Legs, ("ui/uld/Character_hr1.tex", new Vector2(0.7500f, 0.3273f), new Vector2(0.8750f, 0.4727f)) },
+        { HumanSlot.Feet, ("ui/uld/Character_hr1.tex", new Vector2(0.0000f, 0.4727f), new Vector2(0.1250f, 0.6182f)) },
+        { HumanSlot.Ears, ("ui/uld/Character_hr1.tex", new Vector2(0.1250f, 0.4727f), new Vector2(0.2500f, 0.6182f)) },
+        { HumanSlot.Neck, ("ui/uld/Character_hr1.tex", new Vector2(0.2500f, 0.4727f), new Vector2(0.3750f, 0.6182f)) },
+        { HumanSlot.Wrists, ("ui/uld/Character_hr1.tex", new Vector2(0.3750f, 0.4727f), new Vector2(0.5000f, 0.6182f)) },
+        { HumanSlot.RFinger, ("ui/uld/Character_hr1.tex", new Vector2(0.5000f, 0.4727f), new Vector2(0.6250f, 0.6182f)) },
+        { HumanSlot.LFinger, ("ui/uld/Character_hr1.tex", new Vector2(0.5000f, 0.4727f), new Vector2(0.6250f, 0.6182f)) },
+        { HumanSlot.Face, ("ui/uld/Character_hr1.tex", new Vector2(0.0000f, 0.8545f), new Vector2(0.1250f, 1.0000f)) },
+    };
+    
     public static void Draw(HumanSlot slot, EquipItem equipItem) {
         var size = new Vector2(ImGui.GetTextLineHeight() * 2 + ImGui.GetStyle().FramePadding.Y * 4 + ImGui.GetStyle().ItemSpacing.Y);
         using (ImRaii.Group()) {
@@ -45,14 +28,15 @@ public static class ItemIcon {
                 var tex = TextureProvider.GetFromGameIcon(equipItem.IconId.Id).GetWrapOrEmpty();
                 ImGui.Image(tex.ImGuiHandle, size);
             } else {
-                if (EmptyIcons.TryGetValue(slot, out var emptyDrawAction)) {
-                    emptyDrawAction(slot, equipItem, size);
-                    return;
-                }
-
                 ImGui.Dummy(size);
                 var dl = ImGui.GetWindowDrawList();
                 dl.AddRectFilled(ImGui.GetItemRectMin(), ImGui.GetItemRectMax(), ImGui.GetColorU32(ImGuiCol.FrameBg), size.X * 0.15f, ImDrawFlags.RoundCornersAll);
+                if (EmptyIcons.TryGetValue(slot, out var emptyIcon)) {
+                    var tex = TextureProvider.GetFromGame(emptyIcon.TexturePath).GetWrapOrDefault();
+                    if (tex != null) {
+                        dl.AddImage(tex.ImGuiHandle, ImGui.GetItemRectMin(), ImGui.GetItemRectMax(), emptyIcon.uvMin, emptyIcon.uvMax);
+                    }
+                }
             }
 #if DEBUG
             if (ImGui.GetIO().KeyAlt && ImGui.GetIO().KeyShift) {
